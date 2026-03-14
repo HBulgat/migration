bffa9bc1b61d8a31b65eb91805c06d6e2737d4cd
xiexianxi.h <xiexianxi.h@dcarlife.com>
2026-03-14 23:17:25 +0800

    fix: fix

--- migration-diff-service/src/test/java/top/bulgat/migration/diff/application/service/Test.java
+++ migration-diff-service/src/test/java/top/bulgat/migration/diff/application/service/Test.java
@@ -10,7 +10,6 @@ import static org.mockito.Mockito.when;
 
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import top.bulgat.common.base.exception.BizException;
 import top.bulgat.common.base.exception.ErrorCode;
